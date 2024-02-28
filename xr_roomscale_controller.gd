58f3c2d45c0ba694bea21ca5cb3822e966ecf953
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2024-02-28 17:01:57 -0500

    Refactor roomscale controller
    -use variables that are aligned with those used in the xr_scene script for xr components
    
    -delete unused code for controlling character body with stick input as that will always be handled by the underlying game

--- xr_roomscale_controller.gd
+++ xr_roomscale_controller.gd
@@ -1,29 +1,29 @@
-# Credit to demo by Bastiaan Olij
+# Credit to demo by Bastiaan Olij: https://github.com/godotengine/godot-demo-projects/tree/master/xr/openxr_character_centric_movement
 # This script enables room scale VR movement when there is a valid characterbody3D and the user has enabled roomscale option
 
 extends Node
-# Settings to control the character
-@export var rotation_speed : float = 1.0
-@export var movement_speed : float = 5.0
-@export var movement_acceleration : float = 5.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 # Helper variables to keep our code readable
-var origin_node : XROrigin3D = null
-var camera_node : XRCamera3D = null
-var neck_position_node : Node3D = null
+var xr_origin_3D : XROrigin3D = null
+var xr_camera_3D : XRCamera3D = null
+var xr_neck_position_3D : Node3D = null
 
-var current_characterbody3D : CharacterBody3D = null: set = set_characterbody3D
+# Node driving the player movement
+var current_characterbody3D : CharacterBody3D = null
+
+# Whether roomscale movement is presently enabled
 var enabled : bool = false
+
+# Currently unused - node for blacking out screen when player walks to where they should not in roomscale
 #@onready var black_out : Node3D = $XROrigin3D/XRCamera3D/BlackOut
 
 # `recenter` is called when the user has requested their view to be recentered.
 # The code here assumes the player has walked into an area they shouldn't be
 # and we return the player back to the character body.
-# But other strategies can be applied here as well such as returning the player
-# to a starting position or a checkpoint.
+
 func _ready():
 	set_enabled(false, null)
 	print("Current gravity detected in xr roomscale node is: ", gravity)
@@ -33,47 +33,37 @@ func recenter() -> bool:
 	var new_camera_transform : Transform3D = current_characterbody3D.global_transform
 
 	# Set to the height of our neck joint
-	new_camera_transform.origin.y = neck_position_node.global_position.y
+	new_camera_transform.origin.y = xr_neck_position_3D.global_position.y
 
 	# Apply transform our our next position to get our desired camera transform
-	new_camera_transform = new_camera_transform * neck_position_node.transform.inverse()
+	new_camera_transform = new_camera_transform * xr_neck_position_3D.transform.inverse()
 
 	# Remove tilt from camera transform
-	var camera_transform : Transform3D = camera_node.transform
+	var camera_transform : Transform3D = xr_camera_3D.transform
 	var forward_dir : Vector3 = camera_transform.basis.z
 	forward_dir.y = 0.0
 	camera_transform = camera_transform.looking_at(camera_transform.origin + forward_dir.normalized(), Vector3.UP, true)
 
 	# Update our XR location
-	origin_node.global_transform = new_camera_transform * camera_transform.inverse()
+	xr_origin_3D.global_transform = new_camera_transform * camera_transform.inverse()
 
+	# Return true in case calling code needs to know when we are done (could also use a signal here, but given the speed at which this needs to be done returning true instead)
 	return true
-# `_get_movement_input` returns our move input by querying the move action on each controller
-#func _get_movement_input() -> Vector2:
-	#var movement : Vector2 = Vector2()
 
-	# If move is not bound to one of our controllers,
-	# that controller will return a Vector2(0.0, 0.0)
-	#movement += $XROrigin3D/LeftHand.get_vector2("move")
-	#movement += $XROrigin3D/RightHand.get_vector2("move")
-
-	#return movement
-
+	
 # `_process_on_physical_movement` handles the physical movement of the player
 # adjusting our character body position to "catch up to" the player.
-# If the character body encounters an obstruction our view will black out
-# and we will stop further character movement until the player physically
-# moves back.
+
 func _process_on_physical_movement(delta) -> bool:
 	# If we don't have a current character body, current xr_origin or xr_camera, return
-	if !is_instance_valid(current_characterbody3D) or !is_instance_valid(origin_node) or !is_instance_valid(camera_node):
+	if !is_instance_valid(current_characterbody3D) or !is_instance_valid(xr_origin_3D) or !is_instance_valid(xr_camera_3D):
 		return false
 	
 	# Remember our current velocity, we'll apply that later
 	var current_velocity = current_characterbody3D.velocity
 
 	# Start by rotating the player to face the same way our real player is
-	var camera_basis: Basis = origin_node.transform.basis * camera_node.transform.basis
+	var camera_basis: Basis = xr_origin_3D.transform.basis * xr_camera_3D.transform.basis
 	var forward: Vector2 = Vector2(camera_basis.z.x, camera_basis.z.z)
 	var angle: float = forward.angle_to(Vector2(0.0, 1.0))
 
@@ -81,11 +71,11 @@ func _process_on_physical_movement(delta) -> bool:
 	current_characterbody3D.transform.basis = current_characterbody3D.transform.basis.rotated(Vector3.UP, angle)
 
 	# Reverse this rotation our origin node
-	origin_node.transform = Transform3D().rotated(Vector3.UP, -angle) * origin_node.transform
+	xr_origin_3D.transform = Transform3D().rotated(Vector3.UP, -angle) * xr_origin_3D.transform
 
 	# Now apply movement, first move our player body to the right location
 	var org_player_body: Vector3 = current_characterbody3D.global_transform.origin
-	var player_body_location: Vector3 = origin_node.transform * camera_node.transform * neck_position_node.transform.origin
+	var player_body_location: Vector3 = xr_origin_3D.transform * xr_camera_3D.transform * xr_neck_position_3D.transform.origin
 	player_body_location.y = 0.0
 	player_body_location = current_characterbody3D.global_transform * player_body_location
 
@@ -94,10 +84,10 @@ func _process_on_physical_movement(delta) -> bool:
 
 	# Now move our XROrigin back
 	var delta_movement = current_characterbody3D.global_transform.origin - org_player_body
-	origin_node.global_transform.origin -= delta_movement
+	xr_origin_3D.global_transform.origin -= delta_movement
 
 	# Negate any height change in local space due to player hitting ramps etc.
-	origin_node.transform.origin.y = 0.0
+	xr_origin_3D.transform.origin.y = 0.0
 
 	# Return our value
 	current_characterbody3D.velocity = current_velocity
@@ -113,42 +103,11 @@ func _process_on_physical_movement(delta) -> bool:
 		#black_out.fade = 0.0
 		return false
 
-# `_process_movement_on_input` handles movement through controller input.
-# We first handle rotating the player and then apply movement.
-# We also apply the effects of gravity at this point.
-#func _process_movement_on_input(is_colliding, delta):
-	#if !is_colliding:
-		# Only handle input if we've not physically moved somewhere we shouldn't.
-		#var movement_input = _get_movement_input()
-
-		# First handle rotation, to keep this example simple we are implementing
-		# "smooth" rotation here. This can lead to motion sickness.
-		# Adding a comfort option with "stepped" rotation is good practice but
-		# falls outside of the scope of this demonstration.
-		#rotation.y += -movement_input.x * delta * rotation_speed
-
-		# Now handle forward/backwards movement.
-		# Straffing can be added by using the movement_input.x input
-		# and using a different input for rotational control.
-		# Straffing is more prone to motion sickness.
-		#var direction = global_transform.basis * Vector3(0.0, 0.0, -movement_input.y) * movement_speed
-		#if direction:
-			#velocity.x = move_toward(velocity.x, direction.x, delta * movement_acceleration)
-			#velocity.z = move_toward(velocity.z, direction.z, delta * movement_acceleration)
-		#else:
-			#velocity.x = move_toward(velocity.x, 0, delta * movement_acceleration)
-			#velocity.z = move_toward(velocity.z, 0, delta * movement_acceleration)
-
-	# Always handle gravity
-	#velocity.y -= gravity * delta
-
-	#move_and_slide()
-
 # _physics_process handles our player movement.
 func _physics_process(delta):
 	var is_colliding = _process_on_physical_movement(delta)
-	#_process_movement_on_input(is_colliding, delta)
 	
+# Function to allow changing of the relevant characterbody3D that drives the xr origin's movement	
 func set_characterbody3D(new_characterbody3D : CharacterBody3D):
 	if new_characterbody3D == null:
 		print("Game sent null characterbody3d to roomscale")
@@ -159,18 +118,21 @@ func set_characterbody3D(new_characterbody3D : CharacterBody3D):
 	else:
 		current_characterbody3D = new_characterbody3D
 
+# Function to enable or disable roomscale movement
 func set_enabled(value:bool, new_origin) -> bool:
 	if value == true and (current_characterbody3D == null or !is_instance_valid(current_characterbody3D)):
 		print("Tried to enable roomscale but characterbody3D still not set or is set to an invalid instance.")
 		return false
 	if value == true and new_origin.is_class("XROrigin3D"):
 		print("setting new origin3d in roomscale node")
-		origin_node = new_origin
-		camera_node = new_origin.get_node("XRCamera3D")
-		neck_position_node = camera_node.get_node("Neck")
+		xr_origin_3D = new_origin
+		xr_camera_3D = new_origin.get_node("XRCamera3D")
+		xr_neck_position_3D = xr_camera_3D.get_node("Neck")
 	enabled = value
 	set_process(value)
 	set_physics_process(value)
 	if enabled == false:
 		current_characterbody3D = null
+	
+	# Return true in case calling method needs to know we finished; again could use signal here but this is simpler
 	return true
