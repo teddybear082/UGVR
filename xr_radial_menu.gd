97e5f83424c24c784cee64525cf3287cd72f2e1c
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2024-03-04 21:04:13 -0500

    Add color to indicate selected option on hand radial menu

--- xr_radial_menu.gd
+++ xr_radial_menu.gd
@@ -15,23 +15,42 @@ var radial_menu_open_position : Transform3D
 var menu_quads : Array = []
 var last_selected = null
 var handling_input : bool = false
-
-
+var menu_entry_material : StandardMaterial3D = StandardMaterial3D.new()
+var selected_material : StandardMaterial3D = StandardMaterial3D.new()
+var already_added_meshes : bool = false
 
 func _ready():
 	hide()
 	add_child(anchor)
+	
+	# Set default menu entry background material
+	menu_entry_material.albedo_color = Color.BLACK
+	menu_entry_material.disable_ambient_light = true
+	menu_entry_material.disable_receive_shadows = true
+	menu_entry_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	
+	# Set color for selected radial menu items
+	selected_material.albedo_color = Color.GRAY
+	selected_material.disable_ambient_light = true
+	selected_material.disable_receive_shadows = true
+	selected_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 	#print("Made it to after hide function in xr radial menu")
 
 func set_menu_entries(entries : Array) -> void:
+	# Prevent doing this more than once per session
+	if already_added_meshes:
+		return
+	# Set up menu entries list and associated meshes around radial menu
 	menu_entries = entries
 	for entry in menu_entries:
 		add_entry(entry)
-		
+	already_added_meshes = true
+
 func add_entry(entry: String) -> void:
 	#print("Made it to add entry function in xr radial menu")
 	var mesh_instance : MeshInstance3D = MeshInstance3D.new()
 	var mesh : QuadMesh = QuadMesh.new()
+	mesh_instance.material_override = menu_entry_material
 	mesh.size = Vector2(0.15, 0.15)
 	mesh_instance.mesh = mesh
 	var mesh_label : Label3D = Label3D.new()
@@ -42,6 +61,7 @@ func add_entry(entry: String) -> void:
 	mesh_label.width = 100.0
 	mesh_label.autowrap_mode = 2
 	mesh_instance.add_child(mesh_label)
+	mesh_label.transform.origin.z = 0.001
 	mesh_instance.show()
 	anchor.add_child(mesh_instance)
 	menu_quads.append(mesh_instance)
@@ -56,6 +76,7 @@ func add_entry(entry: String) -> void:
 func unselect(entry: String) -> void:
 	var index = menu_entries.find(entry)
 	var mesh = menu_quads[index]
+	mesh.material_override = menu_entry_material
 
 func select(entry: String) -> void:
 	if last_selected != null:
@@ -64,6 +85,7 @@ func select(entry: String) -> void:
 
 	var index = menu_entries.find(entry)
 	var mesh = menu_quads[index]
+	mesh.material_override = selected_material
 	last_selected = entry
 
 func entry_from_position(position: Vector3) -> String:
