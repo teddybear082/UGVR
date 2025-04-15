48ca2c2fcddb57e26f0e5ba4f1773a64d4370053
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2025-04-15 15:47:26 -0400

    add more custom game script default scripts

--- xr_injector/custom_scripts/custom_game_script.gd
+++ xr_injector/custom_scripts/custom_game_script.gd
@@ -7,9 +7,9 @@ var right_controller : XRController3D
 # Convenience VR Camera / HMD reference, do not modify, will be set in xr_scene.gd automatically
 var hmd : XRCamera3D
 # Convenience reference to Primary (weapon/turning hand) controller set by user, do not modify, will be set in xr_scene.gd automatically
-var primary_controller : XRController3D
+var primary_controller
 # Convenience reference to Secondary (movement/off-hand) controller set by user, do not modify, will be set in xr_scene.gd automatically
-var secondary_controller : XRController3D
+var secondary_controller
 # Convenience XR Scene reference (the parent node of all of UGVR), do not modify, will be set in xr_scene.gd
 var xr_scene : Node3D = null
 # Convenience reference to the node at the top of the scene tree in any game, allows finding or getting other nodes in game scene tree
@@ -18,15 +18,34 @@ var scene_root = null
 var active_flat_screen_camera3D = null
 # Track whether single use function has already been called
 var on_xr_setup_already_run : bool = false
+# Basic clear 2D shader (for use in trying to replace problematic canvas item shaders)
+var default_2d_transparent_shader = """
+shader_type canvas_item;
+
+void fragment(){
+  COLOR = vec4(1.0, 1.0, 1.0, 0.0);
+}
+"""
+# Basic clear 3D mesh shader (for use in trying to replace problematic mesh shaders)
+var default_3d_transparent_mesh_shader = """
+shader_type spatial;
+render_mode unshaded, transparent;
+
+void fragment() {
+	ALBEDO = vec3(1.0); // RGB = white
+	ALPHA = 0.0;        // Fully transparent
+}
+"""
+
 # Put game specific custom variables here for your code, e.g., var game_name_important_node = null
 
 
-# Called when the node enters the scene tree for the first time.  Can't use controller references yet as they will not be set up yet.
+# Called when the node enters the scene tree for the first time.  Can't use convenience references yet as they will not be set up yet.
 func _ready():
 	pass
 
 # Called only once after xr scene and all convenience variables are set, insert any code you want to run then here
-# Note that you can now access any of the xr scene variables directly, example: xr_scene.xr_pointer.enabled=false
+# Note that you can now access any of the xr scene variables directly at this point, example: xr_scene.xr_pointer.enabled=false
 func _on_xr_setup_run_once():
 	pass
 
@@ -121,10 +140,10 @@ func reparent_game_node_to_controller(game_node : Node3D, controller : XRControl
 	remote_transform.remote_path = game_node.get_path()
 
 # Convenience function to move game node to track HMD, may not work in all instances, offset is x, y, z relative to HMD
-func reparent_game_node_to_hmd(game_node : Node3D, hmd : XRCamera3D, offset: Vector3 = Vector3(0,0,0)) -> void:
+func reparent_game_node_to_hmd(game_node : Node3D, hmd_node : XRCamera3D, offset: Vector3 = Vector3(0,0,0)) -> void:
 	var remote_transform : RemoteTransform3D = RemoteTransform3D.new()
 	var node_holder : Node3D = Node3D.new()
-	hmd.add_child(node_holder)
+	hmd_node.add_child(node_holder)
 	node_holder.transform.origin = offset
 	node_holder.add_child(remote_transform)
 	remote_transform.update_scale = false
@@ -135,14 +154,29 @@ func remove_shader_from_mesh(mesh_node : MeshInstance3D)-> void :
 	var surface_material = mesh_node.get_surface_override_material(0)
 	if surface_material:
 		if surface_material.is_class("ShaderMaterial"):
-			surface_material.get_shader().set_code("")
+			surface_material.get_shader().set_code(default_3d_transparent_mesh_shader)
 
 # Convenience funcrion to try to delete a problematic shader from a 2D canvas object (like a rectangle on the screen), may cause game crashes if game code depends on setting variables in the shader
 func remove_shader_from_UI_object(canvas_item : CanvasItem)-> void :
 	var surface_material = canvas_item.material
 	if surface_material:
 		if surface_material.is_class("ShaderMaterial"):
-			surface_material.get_shader().set_code("")
+			surface_material.get_shader().set_code(default_2d_transparent_shader)
+
+# Convenience function to completely remove a potential problematic material from a mesh.  May have unintended consequences or create unwarranted visual artifacts.
+func remove_material_from_mesh(mesh_node : MeshInstance3D) -> void:
+	var number_of_surface_materials = mesh_node.get_surface_override_material_count()
+	for i in range(number_of_surface_materials):
+		mesh_node.set_surface_override_material(i, null)
+	var mesh = mesh_node.mesh	
+	if mesh:
+		var mesh_surface_count = mesh.get_surface_count()
+		for i in range(mesh_surface_count):
+			mesh.surface_set_material(i, null)
+
+# Convenience function to completely remove a potential problematic material from a 2D item (like a UI item).  May have unintended consequences or create unwarranted visual artifacts.	
+func remove_material_from_UI_object(canvas_item : CanvasItem) -> void:
+	canvas_item.material = null
 
 # Convenience function to hide a node.  May need to be run in _process if other game code may dynamically hide and show the element
 func hide_node(node : Node) -> void:
@@ -150,22 +184,36 @@ func hide_node(node : Node) -> void:
 		node.hide()
 	else:
 		for property_dictionary in node.get_property_list():
-			if "visible" in property_dictionary.name:
+			if "visible" in property_dictionary["name"]:
 				node.visible = false
 				break
 
 # Convenience function to show a hidden node. May need to be run in _process if other game code may dynamically hide and show the element
 func show_node(node : Node) -> void:
-	node.show()
-	node.visible = true
 	if node.has_method("show"):
 		node.show()
 	else:
 		for property_dictionary in node.get_property_list():
-			if "visible" in property_dictionary.name:
+			if "visible" in property_dictionary["name"]:
 				node.visible = true
 				break
 
+# Convenience function to print a scene tree of the scene at the time the function is called to the existing log for the game
+# The log is usually found in Users/userscomputername/AppData/Roaming/Godot/app_userdata/game_name, but some games maintain their user data with logs in other folders
+# This can be used to find potential nodes for reparenting, adjusting shaders, etc., without having to try to decompile the flat screen game
+func print_scene_tree_pretty_to_game_log() -> void:
+	get_tree().current_scene.print_tree_pretty()
+
+# Convenience function to print a scene tree of the scene at the time the function is called to a text file dedicated to this purpose in the XRConfigs folder.  Note that by default the file does NOT clear.  Set to true instead when calling the function to just print the latest scene tree to the file.
+# This can be used to find potential nodes for reparenting, adjusting shaders, etc., without having to try to decompile the flat screen game
+func print_scene_tree_pretty_to_text_file(full_file_path : String = "", clear_previous_contents : bool = false) -> void:
+	var text_file = full_file_path if full_file_path != "" else xr_scene.xr_config_handler.cfg_base_path + "/xr_scene_tree_print_log.txt"
+	if clear_previous_contents == true and FileAccess.file_exists(text_file):
+		OS.move_to_trash(text_file)
+	var file = FileAccess.open(text_file, FileAccess.WRITE)
+	var node_tree_string = get_tree().current_scene.get_tree_string_pretty() + "\n\n"
+	file.store_string(node_tree_string)
+	
 # Setter function for xr_scene reference, called in xr_scene.gd automatically
 func set_xr_scene(new_xr_scene) -> void:
 	xr_scene = new_xr_scene
